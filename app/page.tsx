737c9a50999256dae7a266bcdcde4a81f4bee5d4
Benjamin Jeon <baekhakjeon@gmail.com>
2024-02-26 13:19:36 -0800

    이미지 크기 조정 및 스타일 변경

--- app/page.tsx
+++ app/page.tsx
@@ -32,7 +32,7 @@ export default function Home() {
                   src={image}
                   width={500}
                   height={500}
-                  className="h-full object-cover object-center"
+                  className="h-full w-full object-cover object-center"
                   alt="Models"
                 />
               </div>
@@ -40,15 +40,9 @@ export default function Home() {
           ))}
         </CarouselContent>
       </Carousel>
-      {/* <div className="container absolute inset-0 h-[60vh] w-[100vw] md:h-[100vh]">
-        <Image
-          src="https://images.unsplash.com/photo-1604537529428-15bcbeecfe4d?q=80&w=3538&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDF8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D"
-          fill
-          className="object-cover object-center"
-          alt="Models"
-        />
-      </div> */}
-      <div className="mt-[48vh] flex flex-col p-8 md:mt-[87vh] md:flex-row">
+      {/* Dummy div tag for below paragraph */}
+      <div className="container h-[calc(60vh-115px)] w-[100vw] md:h-[calc(100vh-115px)]" />
+      <div className="flex flex-col p-8 md:flex-row">
         <p className="text-md md:w-[60%] md:text-2xl">
           AVIE MUAH IS AN ARTISAN CLOTHING BRAND BASED ON THE UNIQUE TASTES OF
           ITS FOUNDER.THE BRAND USES NATURAL FIBERS TO CREATE COMFORTABLE
